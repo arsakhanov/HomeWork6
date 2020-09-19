964c98dd38a1b1bc191a95635a23ea939b8a0d6b
arsakhanov <baki_12_95@mail.ru>
2020-09-19 21:33:14 +0300

    +теперь сервер отвечает на запросы

--- src/ru/arsakhanov/Server.java
+++ src/ru/arsakhanov/Server.java
@@ -3,7 +3,6 @@ package ru.arsakhanov;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 /**
  * класс для создания сервера на локальном хосте
@@ -12,6 +11,7 @@ public class Server {
 
     private static final File dir = new File("E://InnoProjects/HomeWork6");
     RecursionFiles recursionFiles = new RecursionFiles();
+    private static final String NOT_FOUND = "No response for POST request";
 
     /**
      * Метод выполяет функцию сервера на локальном хосте
@@ -22,14 +22,22 @@ public class Server {
     public void startServer() {
         try (ServerSocket server = new ServerSocket(8040)) {
             System.out.println("Server is started");
-            Socket clientCocket = server.accept();
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientCocket.getInputStream()))) {
-                String url = getUrlRequest(in);
-                if (url.equals("POST")) {
-                    sendRequest();
-                } else {
-                    recursionFiles.recursionDirectory(dir, 0);
+            while (true) {
+                Socket clientCocket = server.accept();
+                try (BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientCocket.getInputStream()));
+                     PrintWriter out = new PrintWriter((clientCocket.getOutputStream()))) {
+
+                    String url = getUrlRequest(in);
+                    if (url.equals("GET")) {
+                        String showDirectory = sendRequest(recursionFiles.recursionDirectory(dir));
+                        out.println(showDirectory);
+                        out.flush();
+                    } else {
+                        String error = sendRequest(NOT_FOUND);
+                        out.println(error);
+                        out.flush();
+                    }
                 }
             }
         } catch (IOException e) {
@@ -44,21 +52,24 @@ public class Server {
      * @return возращает первое слово в адресной строке,
      * а именно GET или POST
      */
-    private String getUrlRequest(BufferedReader input) {
-        var reader = new Scanner(input).useDelimiter("\r\n");
-        var line = reader.next();
-        return line.split(" ")[0];
+    private String getUrlRequest(BufferedReader input) throws IOException {
+        String reader = input.readLine();
+        return reader.split(" ")[0];
     }
 
     /**
-     * метод, который просто выводит на консоль "ошибку"
-     * для POST запроса
+     * Метод возвращает строку с правильным опианием http ответа
+     *
+     * @param response параметр типа String - указывается что именно нужно передать для ответа на запрос
+     * @return возвращает строку с описанием http ответа и параметром текста для ответа
      */
-    private void sendRequest() {
-        System.out.println("HTTP/1.1 404 Not Found");
-        System.out.println("Content-Type: text/html; charset=utf-8");
-        System.out.println("No response for POST request");
+    public String sendRequest(String response) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("HTTP/1.1 404 Not Found\n");
+        stringBuilder.append("Content-Type: text/html; charset=utf-8\n\n");
+        stringBuilder.append(response);
+        return new String(stringBuilder);
     }
 
 }
-
+//
